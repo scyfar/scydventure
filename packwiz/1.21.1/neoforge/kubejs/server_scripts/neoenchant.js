f0cd0ef9b57b8116b9ceb3d59e7ec2ebc7949bfc
Scyfar <scyfar@pm.me>
2025-03-14 21:01:36 +0100

    fix: make missing enchants available
    All the added enchants are available as chest loot with a 5% chance.

---
+++ packwiz/1.21.1/neoforge/kubejs/server_scripts/neoenchant.js
@@ -0,0 +1,27 @@
+// https://github.com/Hardel-DW/NeoEnchant
+LootJS.lootTables((event) => {
+  event.modifyLootTables(LootType.CHEST).createPool((pool) => {
+    pool.when((c) => c.randomChance(0.05));
+    pool.addEntry(
+      LootEntry.of("minecraft:enchanted_book")
+        .withWeight(10)
+        .enchantRandomly(["enchantplus:boots/step_assist"])
+    );
+
+    pool.addEntry(
+      LootEntry.of("minecraft:enchanted_book")
+        .withWeight(1)
+        .enchant((builder) => {
+          builder.withEnchantment("enchantplus:pickaxe/spawner_touch", 1);
+        })
+    );
+
+    pool.addEntry(
+      LootEntry.of("minecraft:enchanted_book")
+        .withWeight(1)
+        .enchant((builder) => {
+          builder.withEnchantment("enchantplus:sword/pull", 1);
+        })
+    );
+  });
+});
