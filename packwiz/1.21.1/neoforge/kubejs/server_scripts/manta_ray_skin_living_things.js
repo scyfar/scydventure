0788cb0253e25fc8562299e425bf60f066b4d65e
Scyfar <scyfar@pm.me>
2025-04-14 16:38:40 +0200

    refactor: integrate beta 0.2.0 changes
    The base game is considered ready for release now. Advanced gameplay
    such as the new dimension has not been tested but it is considered out
    of scope for now.
    
    The state of Scydventure is good enough for now and later stages may be
    tweaked with later releases.
    
    Closes: #74

--- packwiz/1.21.1/neoforge/kubejs/server_scripts/manta_ray_skin_living_things.js
+++
@@ -1,6 +0,0 @@
-LootJS.lootTables((event) => {
-  event.modifyEntityTables("livingthings:mantaray").createPool((pool) => {
-    pool.when((c) => c.randomChance(0.15));
-    pool.addEntry(LootEntry.of("scydventure:manta_ray_skin").setCount([1, 3]));
-  });
-});
